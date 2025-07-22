bd4e545e25a363437923f104e20908fe8dc9bc13
TarinaSnyman <tarina.snyman@gmail.com>
2025-07-22 10:50:06 +0200

    Initial Commit
    Created initial jsp, servlet and timersetting

---
+++ src/java/TimerSettings.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tarina
+ */
+public class TimerSettings {
+    //sets default value
+    private int studyMinutes=25;
+    private int breakMinutes=5;
+    
+    public int getStudyMinutes()
+    {
+        return studyMinutes;
+    }
+    public int getBreakMinutes()
+    {
+        return breakMinutes;
+    }
+    
+}
---
+++ web/index.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : index
+    Created on : 22 Jul 2025, 10:36:24
+    Author     : tarin
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%
+    int studyMinutes=(int)
+%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Study Timer</title>
+    </head>
+    <body>
+        <h2>Welcome to the Study Timer</h2>
+        
+        
+        <p>To set your own timer please sign in or register</p>
+    </body>
+</html>
